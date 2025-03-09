aab3f7dab95a20bfc31445d79e51bb74405619e7
alekseishmidko <alexeishmidko@gmail.com>
2025-03-10 01:20:46 +0400

    credentials

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { Request, Response } from 'express';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -8,7 +18,18 @@ export class AuthController {
 
   @Post('register')
   @HttpCode(HttpStatus.OK)
-  public async register(@Body() dto: RegisterDto) {
-    return this.authService.register(dto);
+  public async register(@Body() dto: RegisterDto, @Req() req: Request) {
+    return this.authService.register(dto, req);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  public async login(@Body() dto: LoginDto, @Req() req: Request) {
+    return this.authService.login(dto, req);
+  }
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  public async logout(@Req() req: Request, @Res() res: Response) {
+    return this.authService.logout(req, res);
   }
 }
